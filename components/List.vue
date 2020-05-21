fbb1d439f642eb540c7cd183ab8cf6c415727b27
Fabian Bucher <git@fabianbucher.de>
2020-05-21 16:36:28 +0000

    remove popularity stat
    * for now, until stats.lineageos.org comes up again

--- components/List.vue
+++ components/List.vue
@@ -36,11 +36,11 @@
                 <a-icon type="tag" /> {{ devices[key].version }}
               </a-tag>
             </a-tooltip>
-            <a-tooltip placement="top" title="Popularity">
+            <!--a-tooltip placement="top" title="Popularity">
               <a-tag color="orange">
                 <a-icon type="pie-chart" /> 0.2%
               </a-tag>
-            </a-tooltip>
+            </a-tooltip-->
             <a-tooltip placement="top" title="Maintainers">
               <a-tag color="purple">
                 <a-icon type="team" /> {{ devices[key].maintainers }}
