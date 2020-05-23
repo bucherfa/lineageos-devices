6bf2d4cc63388faaf2dc7e5552f5632a76c83c22
Fabian Bucher <git@fabianbucher.de>
2020-05-23 22:06:25 +0000

    extend footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,33 @@
 <template>
   <footer class="footer">
     <div class="footer__content">
+      <div class="footer__item">
+        <div>
+          Last updated: {{ updated }}
+        </div>
+        <div>
+          <a :href="homepage">{{ name }}</a> version: {{ version }}
+        </div>
+      </div>
       <!-- eslint-disable-next-line vue/no-v-html -->
-      <div v-html="info" />
+      <div class="footer__item" v-html="info" />
     </div>
   </footer>
 </template>
 
 <script>
 import data from '../static/data.json'
-// eslint-disable-next-line no-console
-console.log()
+import packageJSON from '../package.json'
+
 export default {
   name: 'Footer',
   data () {
     return {
-      info: data.info
+      info: data.info,
+      version: packageJSON.version,
+      homepage: packageJSON.homepage,
+      updated: new Date(data._updated).toISOString().split('.')[0].split('T').join(' '),
+      name: packageJSON.name
     }
   },
   computed: {},
@@ -29,8 +41,21 @@ export default {
 }
 
 .footer__content {
+  display: flex;
   max-width: 75rem;
   margin: 0 auto;
-  padding: 3rem 0.5rem;
+  padding: 3rem 0.25rem;
+}
+
+.footer__item {
+  flex-grow: 1;
+  flex-basis: 0;
+  margin: 0.25rem;
+}
+
+@media only screen and (max-width: 1024px) {
+  .footer__content {
+    flex-direction: column-reverse;
+  }
 }
 </style>
