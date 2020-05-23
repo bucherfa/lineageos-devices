ff84554b5d4290b3a28da5601aeca4c047197900
Fabian Bucher <git@fabianbucher.de>
2020-05-23 22:31:24 +0000

    adjust data script

--- setup/gatherData.sh
+++ setup/gatherData.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# exit on failure
+set -e
 # Backup old data.json
 cp extra/data.json "extra/data.backup.$(date +%F_%R).json"
 # Create dirs
@@ -6,13 +8,12 @@ mkdir -p temp
 # Clone data repo
 git clone https://github.com/LineageOS/lineage_wiki temp/lineage_wiki
 # Run data.json generator script
-node src/gatherData.js
+node setup/gatherData.js
 # Create folder for small device images
 mkdir -p static/devices
 # Scale images
 magick mogrify -geometry x168 temp/lineage_wiki/images/devices/*.png
 # Move images to assets
 cp temp/lineage_wiki/images/devices/*.png static/devices/
-exit 1
 # Remove data repo
 rm -rf temp
