e62e0b25de4b54117e1897d60a9e4775e71a9248
Fabian Bucher <git@fabianbucher.de>
2020-08-23 20:47:30 +0200

    replace curl with wget
    for some reason when trying to download the spreadsheet with curl, it downloads some 404 html page
    by replacing curl with wget it stopped happening
    
    wf

--- setup/spreadsheet.sh
+++ setup/spreadsheet.sh
@@ -4,4 +4,4 @@ SPREADSHEET_FILE=temp/spreadsheet/spreadsheet.csv
 if test -f "$SPREADSHEET_FILE"; then
     cp $SPREADSHEET_FILE "temp/spreadsheet/backup/spreadsheet.$(date +%F_%R).csv"
 fi
-curl "https://docs.google.com/spreadsheets/d/1bx6RvTCEGn5zA06lW_uZGZ_dq6qQyCZC_NifmyeC1lM/export?format=csv&id=1bx6RvTCEGn5zA06lW_uZGZ_dq6qQyCZC_NifmyeC1lM&gid=0" -o $SPREADSHEET_FILE
+wget "https://docs.google.com/spreadsheets/d/1bx6RvTCEGn5zA06lW_uZGZ_dq6qQyCZC_NifmyeC1lM/export?format=csv&id=1bx6RvTCEGn5zA06lW_uZGZ_dq6qQyCZC_NifmyeC1lM&gid=0" -O $SPREADSHEET_FILE
