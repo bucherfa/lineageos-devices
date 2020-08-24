423345f27f973592b5a94bf56c787cec144f43a2
Fabian Bucher <git@fabianbucher.de>
2020-08-24 13:20:20 +0200

    adjust title

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Devices for LineageOS',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
