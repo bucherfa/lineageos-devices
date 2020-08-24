9a377e1b45fa8822c07ae907f1b36a3652c851cf
Fabian Bucher <git@fabianbucher.de>
2020-08-24 13:21:29 +0200

    add html lang attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ module.exports = {
   */
   head: {
     title: 'Devices for LineageOS',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
