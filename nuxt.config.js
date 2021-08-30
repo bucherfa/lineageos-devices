7b12950ffaf42f052eacbb235160e5239f03f39d
Fabian Bucher <git@fabianbucher.de>
2021-08-30 11:56:38 +0200

    add general configuration
    non subdir setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,35 @@ require('dotenv').config()
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/lineageos-devices/'
+    base: '/lineageos-devices/',
+    /*
+    ** Headers of the page
+    */
+    head: {
+      title: 'Devices for LineageOS',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+        { name: 'msapplication-TileColor', content: '#00aba9' },
+        { name: 'msapplication-config', content: '/lineageos-devices/browserconfig.xml' },
+        { name: 'theme-color', content: '#ffffff' }
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/lineageos-devices/apple-touch-icon.png' },
+        { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/lineageos-devices/favicon-32x32.png' },
+        { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/lineageos-devices/favicon-16x16.png' },
+        { rel: 'manifest', href: '/lineageos-devices/site.webmanifest' },
+        { rel: 'mask-icon', color: '#5bbad5', href: '/lineageos-devices/safari-pinned-tab.svg' },
+        { rel: 'shortcut icon', href: '/lineageos-devices/favicon.ico' },
+        { rel: 'preconnect', href: process.env.ACKEE_SERVER }
+      ]
+    }
   }
-} : {}
-
-module.exports = {
-  ...routerBase,
-  // mode
-  mode: 'universal',
+} : {
   /*
   ** Headers of the page
   */
@@ -24,19 +45,25 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { name: 'msapplication-TileColor', content: '#00aba9' },
-      { name: 'msapplication-config', content: '/lineageos-devices/browserconfig.xml' },
+      { name: 'msapplication-config', content: '/browserconfig.xml' },
       { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/lineageos-devices/apple-touch-icon.png' },
-      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/lineageos-devices/favicon-32x32.png' },
-      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/lineageos-devices/favicon-16x16.png' },
-      { rel: 'manifest', href: '/lineageos-devices/site.webmanifest' },
-      { rel: 'mask-icon', color: '#5bbad5', href: '/lineageos-devices/safari-pinned-tab.svg' },
-      { rel: 'shortcut icon', href: '/lineageos-devices/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
+      { rel: 'shortcut icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: process.env.ACKEE_SERVER }
     ]
-  },
+  }
+}
+
+module.exports = {
+  ...routerBase,
+  // mode
+  mode: 'universal',
   /*
   ** Customize the progress-bar color
   */
